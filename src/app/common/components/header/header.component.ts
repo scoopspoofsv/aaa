aa43c4cd0898dba0c5211beee941305f8613110b
scoopspoofsv <shivam.pandey@tothenew.com>
2020-05-25 13:15:41 +0530

    added dark mode and many more features

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -13,13 +13,17 @@ export class HeaderComponent implements OnInit {
 
   products: Product[]= [];
   items: Item[] = [];
+  isDarkMode = false;
 
   totalItems: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.loadCart()
+    this.loadCart();
+    if (JSON.parse(localStorage.getItem('darkmode'))) {
+      this.toggleTheme();
+    }
   }
 
   loadCart(): void {
@@ -34,6 +38,17 @@ export class HeaderComponent implements OnInit {
 			});
       this.totalItems += item.quantity;
     }
-	}
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkmode', JSON.stringify(this.isDarkMode));
+    document.documentElement.style.setProperty('--background', this.isDarkMode ? '#101313' : '#fff');
+    document.documentElement.style.setProperty('--primary', this.isDarkMode ? '#F2CA7E' : '#733702');
+    document.documentElement.style.setProperty('--light', this.isDarkMode ? '#191919' : '#F2F2F2');
+    document.documentElement.style.setProperty('--white-color', this.isDarkMode ? '#070707' : '#FFF');
+    document.documentElement.style.setProperty('--black-color', this.isDarkMode ? '#fff' : '#070707');
+    document.documentElement.style.setProperty('--medium', this.isDarkMode ? '#0D0D0D' : '#D98566');
+  }
 
 }
